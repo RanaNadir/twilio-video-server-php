a832b7285e7dce43ae9cd99d77418b2f86578d37
RanaNadir <rananadir977@gmail.com>
2017-04-30 15:30:17 +0500

    Update and rename config.example.php to config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+
+<?php
+// see README.md for an explanation of these config params
+$TWILIO_ACCOUNT_SID = 'AC3f649f9e8c0584aa58a63812575c7a87';
+$TWILIO_API_KEY = 'SK9d058d658d06223c16b53fcdf0c418d6';
+$TWILIO_API_SECRET = 'x04oU6HEx8bOtnhbr71Lyp4YFvTD5z1a';
